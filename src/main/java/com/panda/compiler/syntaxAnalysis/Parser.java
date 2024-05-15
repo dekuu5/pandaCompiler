83c62acb944878a89ad0107ac6dcd83aada6a10d
alaa2702 <74565333+alaa2702@users.noreply.github.com>
2024-05-15 23:02:58 +0300

    I don't know what I am doing. I hope I understand it soon

--- src/main/java/com/panda/compiler/syntaxAnalysis/Parser.java
+++ src/main/java/com/panda/compiler/syntaxAnalysis/Parser.java
@@ -1,6 +1,7 @@
 package com.panda.compiler.syntaxAnalysis;
 
 import com.panda.compiler.lexicalAnalysis.Token;
+import com.panda.compiler.lexicalAnalysis.TokenType;
 import com.panda.compiler.lexicalAnalysis.Toknizer;
 
 import java.util.List;
@@ -10,6 +11,77 @@ public class Parser {
     private int current = 0;
     public Parser(List<Token> tokens){
         this.tokens = tokens;
+    }
+    public void parse() {
+        while (!isAtEnd()) {
+            externalDeclaration();
+        }
+    }
+
+    // Parses an external declaration
+    private void externalDeclaration() {
+        if (match(TokenType.FN)) {
+            functionDefinition();
+        } else {
+            declaration();
+        }
+    }
+
+    // Parses a function definition
+    private void functionDefinition() {
+        while (!check(TokenType.LEFT_BRACE)) {
+            declarationSpecifier();
+        }
+        // Parse the rest of the function definition
+    }
+
+    // Parses a declaration specifier
+    private void declarationSpecifier() {
+        if (match(TokenType.TYPE_SPECIFIER)) {
+            // Parse type specifier
+        } else if (match(TokenType.STORAGE_CLASS_SPECIFIER)) {
+            // Parse storage class specifier
+        } else if (match(TokenType.TYPE_QUALIFIER)) {
+            // Parse type qualifier
+        }
+    }
+
+    // Parses a declaration
+    private void declaration() {
+
+    }
+
+    // Utility methods for parsing
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type() == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type() == TokenType.EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
     }
 
 }
