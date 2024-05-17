15f70e562d569db8dd1adb0b2ae59172a7885630
alaa2702 <74565333+alaa2702@users.noreply.github.com>
2024-05-17 20:59:40 +0300

    starting parsing

--- src/main/java/com/panda/compiler/syntaxAnalysis/GrammarRules.java
+++ src/main/java/com/panda/compiler/syntaxAnalysis/GrammarRules.java
@@ -4,74 +4,22 @@ import com.panda.compiler.lexicalAnalysis.Token;
 
 import java.util.HashMap;
 import java.util.List;
-interface RulesVisitor<R> {
-    R visitTranslationUnit(TranslationUnit translationUnit);
-    R visitFunctionDefinition(ExternalDeclaration.FunctionDefinition functionDefinition);
-    R visitDeclarationSpecifier(ExternalDeclaration.VariableDeclaration declarationSpecifier);
-    R visitDeclaration(Declaration declaration);
-    R visitTypeSpecifier(TypeSpecifier typeSpecifier);
 
-    R visitParameterList(ParameterList parameterList);
-
-    R visitJumpStatement(Statement.JumpStatement jumpStatement);
-
-    R visitDeclarator(Declarator declarator);
-
-    R visitParameterDeclaration(ParameterDeclaration parameterDeclaration);
-
-    R visitCompoundStatement(CompoundStatement compoundStatement);
-
-    R visitIFStatement(Statement.ControlFlowStatement.If anIf);
-
-    R visitVariableDeclarationStatement(Statement.VariableDeclarationStatement variableDeclarationStatement);
-
-    R visitIntegerConstant(Constant.IntegerConstant integerConstant);
-
-    R visitFloatConstant(Constant.FloatConstant floatConstant);
-
-    R visitSwitchStatement(Statement.ControlFlowStatement.Switch aSwitch);
-
-    R visitWhileStatement(Statement.ControlFlowStatement.While aWhile);
-
-    R visitForStatement(Statement.ControlFlowStatement.For aFor);
-
-    R visitExpressionStatement(Statement.ExpressionStatement expressionStatement);
-
-    R visitStringConstant(Constant.StringConstant stringConstant);
-
-    R visitCharConstant(Constant.CharConstant charConstant);
-
-    R visitBooleanConstant(Constant.BooleanConstant booleanConstant);
-
-    R visitIdentifier(Identifier identifier);
-
-    R visitLogicalExpression(Expression.LogicalExpression logicalExpression);
-
-    R visitLogicalTerm(Expression.LogicalTerm logicalTerm);
-
-    R visitPrimaryExpression(Expression.PrimaryExpression primaryExpression);
-
-    R visitUnaryExpression(Expression.UnaryExpression unaryExpression);
-
-    R visitMultiplicativeExpression(Expression.MultiplicativeExpression multiplicativeExpression);
-
-    R visitAdditiveExpression(Expression.AdditiveExpression additiveExpression);
-
-    R visitComparison(Expression.Comparison comparison);
-}
 public abstract class GrammarRules {
     public abstract <R> R accept(RulesVisitor<R> visitor);
 }
-    class TranslationUnit extends GrammarRules {
-        final List<ExternalDeclaration> rules;
-        TranslationUnit(List<ExternalDeclaration> rules) {
-            this.rules = rules;
-        }
-        @Override
-        public <R> R accept(RulesVisitor<R> visitor) {
-            return visitor.visitTranslationUnit(this);
-        }
+class TranslationUnit extends GrammarRules {
+    final List<ExternalDeclaration> rules;
+
+    TranslationUnit(List<ExternalDeclaration> rules) {
+        this.rules = rules;
     }
+
+    @Override
+    public <R> R accept(RulesVisitor<R> visitor) {
+        return visitor.visitTranslationUnit(this);
+    }
+}
     abstract class ExternalDeclaration extends GrammarRules {
          static class FunctionDefinition extends ExternalDeclaration {
             final Identifier identifier;
---
+++ src/main/java/com/panda/compiler/syntaxAnalysis/RulesVisitor.java
@@ -0,0 +1,57 @@
+package com.panda.compiler.syntaxAnalysis;
+
+public interface RulesVisitor<R> {
+    R visitTranslationUnit(TranslationUnit translationUnit);
+    R visitFunctionDefinition(ExternalDeclaration.FunctionDefinition functionDefinition);
+    R visitDeclarationSpecifier(ExternalDeclaration.VariableDeclaration declarationSpecifier);
+    R visitDeclaration(Declaration declaration);
+    R visitTypeSpecifier(TypeSpecifier typeSpecifier);
+
+    R visitParameterList(ParameterList parameterList);
+
+    R visitJumpStatement(Statement.JumpStatement jumpStatement);
+
+    R visitDeclarator(Declarator declarator);
+
+    R visitParameterDeclaration(ParameterDeclaration parameterDeclaration);
+
+    R visitCompoundStatement(CompoundStatement compoundStatement);
+
+    R visitIFStatement(Statement.ControlFlowStatement.If anIf);
+
+    R visitVariableDeclarationStatement(Statement.VariableDeclarationStatement variableDeclarationStatement);
+
+    R visitIntegerConstant(Constant.IntegerConstant integerConstant);
+
+    R visitFloatConstant(Constant.FloatConstant floatConstant);
+
+    R visitSwitchStatement(Statement.ControlFlowStatement.Switch aSwitch);
+
+    R visitWhileStatement(Statement.ControlFlowStatement.While aWhile);
+
+    R visitForStatement(Statement.ControlFlowStatement.For aFor);
+
+    R visitExpressionStatement(Statement.ExpressionStatement expressionStatement);
+
+    R visitStringConstant(Constant.StringConstant stringConstant);
+
+    R visitCharConstant(Constant.CharConstant charConstant);
+
+    R visitBooleanConstant(Constant.BooleanConstant booleanConstant);
+
+    R visitIdentifier(Identifier identifier);
+
+    R visitLogicalExpression(Expression.LogicalExpression logicalExpression);
+
+    R visitLogicalTerm(Expression.LogicalTerm logicalTerm);
+
+    R visitPrimaryExpression(Expression.PrimaryExpression primaryExpression);
+
+    R visitUnaryExpression(Expression.UnaryExpression unaryExpression);
+
+    R visitMultiplicativeExpression(Expression.MultiplicativeExpression multiplicativeExpression);
+
+    R visitAdditiveExpression(Expression.AdditiveExpression additiveExpression);
+
+    R visitComparison(Expression.Comparison comparison);
+}
